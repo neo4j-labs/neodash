7beb0caeb3f4ca15ecb9014c1ab6f61535961f84
Bart Broere <b.broere@nfi.nl>
2021-11-25 17:17:40 +0100

    Restore node_modules to babel exclusions

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
         rules: [
             {
                 test: /\.(js|jsx|tsx)$/,
+                exclude: /(node_modules)/,
                 loader: 'babel-loader',
                 options: { presets: ["@babel/env"] }
             },
