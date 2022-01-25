ecc5b9f6fdae4ae1ee1db0ede870c160a4236956
Niels de Jong <niels-121@hotmail.com>
2022-01-25 15:37:30 +0100

    Added maximizing of card settings

--- src/card/settings/CardSettings.tsx
+++ src/card/settings/CardSettings.tsx
@@ -7,15 +7,19 @@ import { CardContent } from '@material-ui/core';
 
 const NeoCardSettings = ({ settingsOpen, query, database, refreshRate, cypherParameters, width, height, type, reportSettings, reportSettingsOpen,
     onQueryUpdate, onSizeUpdate, onRefreshRateUpdate, onCypherParametersUpdate, onRemovePressed, onReportSettingUpdate,
-    onShiftLeftPressed, onShiftRightPressed, onToggleCardSettings, onTypeUpdate, onToggleReportSettings }) => {
+    onShiftLeftPressed, onShiftRightPressed, onToggleCardSettings, onTypeUpdate,
+     onToggleReportSettings, dashboardSettings, expanded, onToggleCardExpand }) => {
 
    
     const cardHeight = 10 + (120 * height) + (78 * Math.floor((height - 1) / 3)) + "px";
 
     const cardSettingsHeader = <NeoCardSettingsHeader
+        expanded={expanded}
+        onToggleCardExpand={onToggleCardExpand}
         onRemovePressed={onRemovePressed}
         onShiftLeftPressed={onShiftLeftPressed}
         onShiftRightPressed={onShiftRightPressed}
+        fullscreenEnabled={dashboardSettings.fullscreenEnabled}
         onToggleCardSettings={onToggleCardSettings} />
 
     // TODO - instead of hiding everything based on settingsopen, only hide the components that slow down render (cypher editor)
@@ -42,9 +46,8 @@ const NeoCardSettings = ({ settingsOpen, query, database, refreshRate, cypherPar
         onToggleReportSettings={onToggleReportSettings}
         onReportSettingUpdate={onReportSettingUpdate}></NeoCardSettingsFooter> : <div></div>;
 
-
     return (
-        <div style={{ overflowY: "auto" }}>
+        <div className={`card-view ${expanded ? "expanded" : ""}`} style={{ overflowY: "auto" }}>
             {cardSettingsHeader}
             <ReportItemContainer style={{ height: cardHeight, marginTop: "-20px" }} >
                 {cardSettingsContent}
