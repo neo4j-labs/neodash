830fa6767cf2f5d936ae18e05050075e12b26f9d
Niels de Jong <niels-121@hotmail.com>
2022-02-12 11:14:55 +0100

    2.0.8 hotfix

--- tools/push-docker-image.bash
+++ tools/push-docker-image.bash
@@ -1,4 +1,4 @@
 docker image tag neodash nielsdejong/neodash
-docker image tag neodash nielsdejong/neodash:2.0.7
+docker image tag neodash nielsdejong/neodash:2.0.8
 docker push nielsdejong/neodash:latest
-docker push nielsdejong/neodash:2.0.7
+docker push nielsdejong/neodash:2.0.8
