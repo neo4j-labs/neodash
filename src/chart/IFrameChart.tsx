0878f6021100a59e62f2f1c0bc9849a299c01b2d
Niels de Jong <niels-121@hotmail.com>
2022-04-08 10:34:00 +0200

    Added replacement of global parameters to iFrame URLs

--- src/chart/IFrameChart.tsx
+++ src/chart/IFrameChart.tsx
@@ -9,8 +9,18 @@ const NeoIFrameChart = (props: ChartProps) => {
     const [extraRecords, setExtraRecords] = React.useState([]);
     // Records are overridden to be a single element array with a field called 'input'.
     const records = props.records;
+    const parameters = props.parameters ? props.parameters : {};
     const passGlobalParameters = props?.settings?.passGlobalParameters
     const url = records[0]["input"];
+
+    // Replaces all global dashboard parameters inside a string with their values.
+    function replaceDashboardParameters(str) {
+        if(!str) return "";
+        Object.keys(parameters).forEach(key => {
+            str = str.replaceAll("$" + key, parameters[key]);
+        });
+        return str;
+    }
 
     if (!url || !(url.startsWith("http://") || url.startsWith("https://"))) {
         return <p style={{ margin: "15px" }}>Invalid iFrame URL. Make sure your url starts with <code>http://</code> or <code>https://</code>.</p>
@@ -18,7 +28,7 @@ const NeoIFrameChart = (props: ChartProps) => {
 
     const mapParameters = records[0]["mapParameters"] || {};
     const queryString = Object.keys(mapParameters).map(key => key + '=' + mapParameters[key]).join('&');
-    return <iframe style={{ width: "100%", border: "none", marginBottom: "-5px", height: "100%", overflow: "hidden" }} src={url + (passGlobalParameters ? "#" + queryString : "")} />;
+    return <iframe style={{ width: "100%", border: "none", marginBottom: "-5px", height: "100%", overflow: "hidden" }} src={replaceDashboardParameters(url) + (passGlobalParameters ? "#" + queryString : "")} />;
 }
 
 export default NeoIFrameChart;
