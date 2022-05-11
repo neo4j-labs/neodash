d0430211295804eff7ab1050d60a9981c3a5521b
aleksandarneo4j <aleksandar.simeunovic@neotechnology.com>
2022-05-11 15:58:24 +0200

    mod

--- scripts/push-docker-image.bash
+++
@@ -1,6 +0,0 @@
-docker build --no-cache --label "version=2.1.0" . -t neodash
-docker image tag neodash nielsdejong/neodash:2.1.0
-docker image tag neodash nielsdejong/neodash:latest
-docker push nielsdejong/neodash:2.1.0
-docker push nielsdejong/neodash:latest
-
--- scripts/upload-to-s3.bash
+++
@@ -1,3 +0,0 @@
-npm run build
-aws s3 rm s3://neodash-test.graphapp.io/ --recursive
-aws s3 sync dist s3://neodash-test.graphapp.io/ --acl public-read
