508f46f47ae2069a2282b4bfab5bce5972f47b38
BennuFire <agudeloharold13@gmail.com>
2022-08-03 19:47:07 +0200

    Feature/choro (#174)
    * Choropleth First Draft
    
    * Debounce param selection
    
    * Debounce param selection
    
    * Debounce param selection
    Choro Impl
    Choro Docs
    
    * Extra Params logic Refined
    FIxes based on Review
    
    * Feature from url
    
    * Colors
    Fix on boolean settings
    
    * Good bye ma lover
    
    * HotFix Selector
    
    * Fixing some parameter names, styling
    
    Co-authored-by: Niels de Jong <niels-121@hotmail.com>

--- src/chart/ParameterSelectionChart.tsx
+++ src/chart/ParameterSelectionChart.tsx
@@ -10,17 +10,37 @@ import NeoField from '../component/field/Field';
  */
 const NeoParameterSelectionChart = (props: ChartProps) => {
 
-    useEffect(() => {
-        debouncedQueryCallback && debouncedQueryCallback(query, { input: inputText }, setExtraRecords);
-    }, [inputText, query]);
-
     const records = props.records;
     const query = records[0]["input"] ? records[0]["input"] : undefined;
     const parameter = props.settings && props.settings["parameterName"] ? props.settings["parameterName"] : undefined;
     const type = props.settings && props.settings["type"] ? props.settings["type"] : undefined;
     const suggestionsUpdateTimeout = props.settings && props.settings["suggestionsUpdateTimeout"] ? props.settings["suggestionsUpdateTimeout"] : 250;
     const setParameterTimeout = props.settings && props.settings["setParameterTimeout"] ? props.settings["setParameterTimeout"] : 1000;
-    
+
+    const currentValue = (props.getGlobalParameter && props.getGlobalParameter(parameter)) ? props.getGlobalParameter(parameter) : "";
+    const [extraRecords, setExtraRecords] = React.useState([]);
+    const [inputText, setInputText] = React.useState(currentValue);
+    const [value, setValue] = React.useState(currentValue);
+
+    useEffect(() => {
+        const queryTimeOut = setTimeout(() => {
+            debouncedQueryCallback && debouncedQueryCallback(query, { input: inputText, ...props.parameters }, setExtraRecords);
+        }, 150);
+        return () => clearTimeout(queryTimeOut);
+    }, [inputText, query, props.parameters]);
+
+
+    useEffect(() => {
+        const timeOutId = setTimeout(() => {
+            if(value == "")
+                debouncedSetGlobalParameter(parameter, undefined);
+            else
+                debouncedSetGlobalParameter(parameter, value);
+
+        }, 250);
+        return () => clearTimeout(timeOutId);
+    }, [value]);
+
     const debouncedQueryCallback = useCallback(
         debounce(props.queryCallback, suggestionsUpdateTimeout),
         [],
@@ -31,11 +51,6 @@ const NeoParameterSelectionChart = (props: ChartProps) => {
         [],
     );
 
-   
-    const currentValue = (props.getGlobalParameter && props.getGlobalParameter(parameter)) ? props.getGlobalParameter(parameter) : "";
-    const [extraRecords, setExtraRecords] = React.useState([]);
-    const [inputText, setInputText] = React.useState(currentValue);
-    const [value, setValue] = React.useState(currentValue);
 
     // In case the components gets (re)loaded with a different/non-existing selected parameter, set the text to the current global parameter value.
     if (query && value != currentValue && currentValue != inputText) {
@@ -53,9 +68,20 @@ const NeoParameterSelectionChart = (props: ChartProps) => {
     const helperText = settings.helperText;
     const clearParameterOnFieldClear = settings.clearParameterOnFieldClear;
 
+    useEffect(() => {
+        const timeOutId = setTimeout(() => {
+            if(value == "")
+                debouncedSetGlobalParameter(parameter, undefined);
+            else
+                debouncedSetGlobalParameter(parameter, value);
+
+        }, 350);
+        return () => clearTimeout(timeOutId);
+    }, [value]);
+
     return <div>
         {type == "Free Text" ?
-            <>
+            <div style={{width: "100%"}}>
                 <NeoField
                     key={"freetext"}
                     label={helperText ? helperText : label + " " + property}
@@ -63,20 +89,18 @@ const NeoParameterSelectionChart = (props: ChartProps) => {
                     value={value}
                     variant="outlined"
                     placeholder={"Enter text here..."}
-                    style={{ maxWidth: "calc(100% - 30px)", marginLeft: "15px", marginTop: "5px" }}
+                    style={{ maxWidth: "calc(100% - 30px)", marginLeft: "15px", marginTop: "5px", width: "calc(100% - 80px)" }}
                     onChange={(newValue) => {
                         // TODO: i want this to be a proper wait instead of triggering on the first character.
                         if (newValue == null && clearParameterOnFieldClear) {
                             setValue("");
-                            debouncedSetGlobalParameter(parameter, undefined);
                         } else {
                             setValue(newValue);
-                            debouncedSetGlobalParameter(parameter, newValue);
                         }
                     }}
                 />
                 {value !== currentValue ? <CircularProgress size={26} style={{marginTop: "20px", marginLeft: "5px"}} /> : <></>}
-            </>
+            </div>
             :
             <Autocomplete
                 id="autocomplete"
@@ -103,9 +127,9 @@ const NeoParameterSelectionChart = (props: ChartProps) => {
                         props.setGlobalParameter(parameter, newValue);
                     }
                 }}
-                renderInput={(params) => <TextField {...params} InputLabelProps={{ shrink: true }} 
-                placeholder="Start typing..." 
-                label={helperText ? helperText : label + " " + property} variant="outlined" />}
+                renderInput={(params) => <TextField {...params} InputLabelProps={{ shrink: true }}
+                                                    placeholder="Start typing..."
+                                                    label={helperText ? helperText : label + " " + property} variant="outlined" />}
             />
         }
     </div>
