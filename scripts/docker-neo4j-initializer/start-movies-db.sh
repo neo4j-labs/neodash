77eb3d777d014cb587aa1cd3b58008b0897bf337
Alfred Rubin <alfredo.rubin@neo4j.com>
2022-10-06 16:52:28 +0200

    Testing Neo4j Container inside Github Actions

--- scripts/docker-neo4j-initializer/start-movies-db.sh
+++ scripts/docker-neo4j-initializer/start-movies-db.sh
@@ -1,3 +1,3 @@
 echo "Loading Dataset"
 cat ./scripts/docker-neo4j-initializer/movies.cypher | docker exec neo4j bin/cypher-shell -u neo4j -p test
-echo "MATCH () RETURN count(*)" | docker exec neo4j --interactive bin/cypher-shell -u neo4j -p test
+echo "MATCH () RETURN count(*)" | docker exec --interactive neo4j bin/cypher-shell -u neo4j -p test
