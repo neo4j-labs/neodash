8191d51327182e7abac274ae514aa8ba00ab5269
Bennu <agudeloharold13@gmail.com>
2022-11-07 13:08:39 +0100

    Error on Codes

--- src/chart/choropleth/ChoroplethMapChart.tsx
+++ src/chart/choropleth/ChoroplethMapChart.tsx
@@ -38,10 +38,11 @@ const NeoChoroplethMapChart = (props: ChartProps) => {
             const value = recordToNative(row.get(selection['value']));
 
             if(!index || index.length != 3 || isNaN(value)){
-                throw "Invalid selection for choropleth chart. Ensure a three letter country code is retrieved together with a value."
+                return data;
+                //throw "Invalid selection for choropleth chart. Ensure a three letter country code is retrieved together with a value."
             }
             data.push({ "id": index, "value": value });
-            return data
+            return data;
         } catch (e) {
             console.error(e);
             return [];
