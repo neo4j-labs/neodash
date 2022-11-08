d91ba84e8904443f0dbabbd36bf578758b414f8c
Niels de Jong <niels-121@hotmail.com>
2022-11-08 16:57:14 +0100

    Added description for radar chart in extension modal

--- src/extensions/ExtensionConfig.ts
+++ src/extensions/ExtensionConfig.ts
@@ -5,7 +5,7 @@ export const EXTENSIONS = {
         author: "Neo4j Labs",
         image: "advanced-visualizations.png",
         enabled: true,
-        description: "Advanced visualizations let you take your dashboard to the next level. This extension adds a sankey chart to visualize flows, three charts to plot hierarchical data (Sunburst, Circle Packing, Treemap) A Gauge Chart to show percentages and a Choropleth map to visualize country-data.",
+        description: "Advanced visualizations let you take your dashboard to the next level. This extension adds a sankey chart to visualize flows, three charts to plot hierarchical data (Sunburst, Circle Packing, Treemap). A Gauge Chart to show percentages, a Radar chart to show radial data, and a Choropleth map to visualize country-data.",
         link: 'https://neo4j.com/labs/neodash/2.2/user-guide'
     },
     "rule-based-styling": {
