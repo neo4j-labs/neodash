64cf0d3f9f78d7d5fd7009797521ff46448d8740
jharris4 <harris.jb@gmail.com>
2022-12-20 06:22:56 -0500

    fix lint errors

--- src/component/editor/CodeEditorComponent.tsx
+++ src/component/editor/CodeEditorComponent.tsx
@@ -3,36 +3,42 @@ import { CypherEditor, CypherEditorProps } from '@neo4j-cypher/react-codemirror'
 import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
 // import { languages } from '@codemirror/language-data';
 
-const markdownExtensions = [markdown({
-    base: markdownLanguage, //Support GFM
+const markdownExtensions = [
+  markdown({
+    base: markdownLanguage, // Support GFM
     // codeLanguages: languages
-  }),];
+  }),
+];
 
-import "@neo4j-cypher/codemirror/css/cypher-codemirror.css";
+import '@neo4j-cypher/codemirror/css/cypher-codemirror.css';
 
-const NeoCodeEditorComponent = ({ value, onChange = (e) => { }, placeholder,
-    editable = true, language = "cypher",
-    style = { width: "100%", height: "auto", border: "1px solid lightgray" } }) => {
+const NeoCodeEditorComponent = ({
+  value,
+  onChange,
+  placeholder,
+  editable = true,
+  language = 'cypher',
+  style = { width: '100%', height: 'auto', border: '1px solid lightgray' },
+}) => {
+  const editorProps: CypherEditorProps = {
+    cypherLanguage: language === 'cypher',
+    readOnly: !editable,
+    placeholder: placeholder,
+    preExtensions: language === 'markdown' ? markdownExtensions : [],
+    value: value,
+    onValueChanged: (val) => {
+      if (editable && onChange) {
+        onChange(val);
+      }
+    },
+    className: 'ReactCodeMirror', // only used by integration tests
+  };
 
-    const editorProps: CypherEditorProps = {
-        cypherLanguage: language === "cypher",
-        readOnly: !editable,
-        placeholder: placeholder,
-        preExtensions: language === "markdown" ? markdownExtensions : [],
-        value: value,
-        onValueChanged: (val, change) => {
-            if (editable) {
-                onChange(val);
-            }
-        },
-        className: "ReactCodeMirror" // only used by integration tests
-    }
-
-    return (
-        <div className={"autosize"} style={style}>
-            <CypherEditor {...editorProps}/>
-        </div>
-    );
+  return (
+    <div className={'autosize'} style={style}>
+      <CypherEditor {...editorProps} />
+    </div>
+  );
 };
 
 export default NeoCodeEditorComponent;
