26965fea06a2c5b3a608586e796a35c5d9f38cb5
Niels de Jong <niels-121@hotmail.com>
2023-01-05 15:33:00 +0100

    Fixed deployment pipeline

--- src/modal/AboutModal.tsx
+++ src/modal/AboutModal.tsx
@@ -10,7 +10,7 @@ import { Button } from '@material-ui/core';
 import BugReportIcon from '@material-ui/icons/BugReport';
 
 export const NeoAboutModal = ({ open, handleClose, getDebugState }) => {
-  const version = '2.2.1-extended';
+  const version = '2.2.1-pro-who';
 
   const downloadDebugFile = () => {
     const element = document.createElement('a');
