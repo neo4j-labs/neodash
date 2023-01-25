bcc14e911ab6a2ffe4d1d32db4e650ba05ae7a3e
Niels de Jong <niels-121@hotmail.com>
2023-01-25 12:09:52 +0100

    Added optional check for welcome modal

--- cypress/integration/start_page.spec.js
+++ cypress/integration/start_page.spec.js
@@ -23,13 +23,17 @@ describe('NeoDash E2E Tests', () => {
     // Navigate to index
     cy.visit('/');
     cy.wait(1000);
-    cy.get('#form-dialog-title').should('contain', 'NeoDash - Neo4j Dashboard Builder');
-    cy.wait(300);
 
-    // Create new dashboard
-    cy.contains('New Dashboard').click();
-    cy.wait(300);
+    cy.get('#form-dialog-title').then(($div) => {
+      const text = $div.text();
+      if (text == 'NeoDash - Neo4j Dashboard Builder⚡') {
+        cy.wait(300);
+        // Create new dashboard
+        cy.contains('New Dashboard').click();
+      }
+    });
 
+    cy.wait(300);
     // If an old dashboard exists in cache, do a check to make sure we clear it.
     // if (cy.contains("Create new dashboard")) {
     //     cy.contains('Yes').click()
