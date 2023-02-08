3ffd113632713c87f027a6c32a9ee28baa95ddeb
aleksandarneo4j <aleksandar.simeunovic@neotechnology.com>
2023-02-08 16:50:51 +0100

    made some movement, almost done with the neodash-editor

--- apps/neodash-editor/src/index.tsx
+++ apps/neodash-editor/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider as ReduxProvider } from 'react-redux';
-import { configureStore } from '../../../packages/neodash-engine/src/store';
+import { configureStore } from './store';
 import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/lib/integration/react';
-import Application from '../../../neodash-engine';
-import '/node_modules/react-grid-layout/css/styles.css';
-import '/node_modules/react-resizable/css/styles.css';
+import Application from './application/Application';
+import '../node_modules/react-grid-layout/css/styles.css';
+import '../node_modules/react-resizable/css/styles.css';
 
 /**
  * Set up the NeoDash application and wrap it in the needed providers.
