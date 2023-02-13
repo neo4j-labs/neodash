d125b27802b7104d96466ed2b45586a104a2e9a1
Alfred Rubin <alfredo.rubin@neo4j.com>
2023-02-13 16:02:10 +0100

    now the parameters are loaded correctly at the start of the dashboard

--- src/chart/ChartUtils.ts
+++ src/chart/ChartUtils.ts
@@ -1,4 +1,5 @@
 import domtoimage from 'dom-to-image';
+import { Date as Neo4jDate } from 'neo4j-driver-core/lib/temporal-types.js';
 
 /**
  * Converts a neo4j record entry to a readable string representation.
@@ -323,9 +324,36 @@ export const processHierarchyFromRecords = (records: Record<string, any>[], sele
   }, []);
 };
 
+/**
+ * Wrapper for empty check logic, to prevent calling writing the same code too many times
+ * @param obj
+ * @returns  Returns True if the input is null, undefined or an empty object
+ */
 export const isEmptyObject = (obj: object) => {
   if (obj == undefined) {
     return true;
   }
   return Object.keys(obj).length == 0;
 };
+
+/**
+ * Checks that the value in input can be casted to Neo4j Bolt Driver Date
+ * @param value
+ * @returns True if it's an object castable to date
+ */
+export function isCastableToNeo4jDate(value: object) {
+  let keys = Object.keys(value);
+  return keys.length == 3 && keys.includes('day') && keys.includes('month') && keys.includes('year');
+}
+
+/**
+ * Casts value in input to Neo4j Date bolt driver. If can't cast, it will throw an error
+ * @param value
+ * @returns Casted value to Neo4j Bolt Driver Date
+ */
+export function castToNeo4jDate(value: object) {
+  if (isCastableToNeo4jDate(value)) {
+    return new Neo4jDate(value.year, value.month, value.day);
+  }
+  throw new Error(`Invalid input for castToNeo4jDate: ${value}`);
+}
