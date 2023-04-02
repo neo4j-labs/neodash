e3506fd6c652cb9803eaa2a64de45c3bf1e1af4a
Niels de Jong <niels-121@hotmail.com>
2023-04-02 16:16:15 +0200

    First version of YML rendering

--- src/chart/json/JSONChart.tsx
+++ src/chart/json/JSONChart.tsx
@@ -3,18 +3,22 @@ import { ChartProps } from '../Chart';
 import { TextareaAutosize } from '@material-ui/core';
 import { CardContent, Chip, IconButton, Tooltip } from '@material-ui/core';
 import WarningIcon from '@material-ui/icons/Warning';
+import YAML from 'yaml';
+
 /**
  * Renders Neo4j records as their JSON representation.
  */
 const NeoJSONChart = (props: ChartProps) => {
-  const { records } = props;
+  const { records, settings } = props;
+  const type = settings && settings.type ? settings.type : 'json';
+  const value = type == 'json' ? JSON.stringify(records, null, 2) : YAML.stringify(records, null, 2);
 
   return (
     <div style={{ marginTop: '0px' }}>
       <TextareaAutosize
         style={{ width: '100%', border: '1px solid lightgray' }}
         className={'textinput-linenumbers'}
-        value={JSON.stringify(records, null, 2)}
+        value={value}
         aria-label=''
         placeholder='Query output should be rendered here.'
       />
