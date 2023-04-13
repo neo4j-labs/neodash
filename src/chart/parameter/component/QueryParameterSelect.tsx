3db2884ae27c84d3f3327cf1a1b634f6031e57d9
Niels de Jong <niels-121@hotmail.com>
2023-04-13 21:31:33 +0200

    Removed unused variables

--- src/chart/parameter/component/QueryParameterSelect.tsx
+++ src/chart/parameter/component/QueryParameterSelect.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import NodePropertyParameterSelectComponent from './NodePropertyParameterSelect';
 import { ParameterSelectProps } from './ParameterSelect';
 
-/**
- * At the moment relationship property selectors are identical to node property selectors.
- * Therefore, just return the node component.
- */
 const QueryParameterSelectComponent = (props: ParameterSelectProps) => {
   return (
     <NodePropertyParameterSelectComponent
