e1b03c7774721ca0b9540c4a2bf27e8f478443cc
Niels de Jong <niels-121@hotmail.com>
2023-04-14 09:34:45 +0200

    Small monorepo fixes

--- apps/gallery/src/index.tsx
+++ apps/gallery/src/index.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import ReactDOM from 'react-dom';
 // We need to include the base CSS in the root of
 // the app so all of our components can inherit the styles
 import '@neo4j-ndl/base/lib/neo4j-ds-styles.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
