8a2b58ce0addf48b4ccded637cc8dac056b3ce2a
Bennu <agudeloharold13@gmail.com>
2023-05-10 11:05:31 +0200

    fix tests

--- cypress/integration/start_page.spec.js
+++ cypress/integration/start_page.spec.js
@@ -26,7 +26,7 @@ describe('NeoDash E2E Tests', () => {
 
     cy.get('#form-dialog-title').then(($div) => {
       const text = $div.text();
-      if (text == 'NeoDash - Neo4j Dashboard Builder⚡') {
+      if (text == 'NeoDash - Neo4j Dashboard Builder') {
         cy.wait(300);
         // Create new dashboard
         cy.contains('New Dashboard').click();
@@ -39,7 +39,7 @@ describe('NeoDash E2E Tests', () => {
     //     cy.contains('Yes').click()
     // }
 
-    cy.get('#form-dialog-title').should('contain', 'NeoDash - Neo4j Dashboard Builder');
+    cy.get('#form-dialog-title').should('contain', 'Connect to Neo4j');
 
     // Connect to Neo4j database
     // cy.get('#protocol').click()
