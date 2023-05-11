9bca10d27c474fe8b7751b6c4de82fe97e8aa11e
aleksandarneo4j <aleksandar.simeunovic@neotechnology.com>
2023-05-11 17:58:13 +0200

    added report types descriptions

--- src/extensions/nlp-query/OpenAiManager.ts
+++ src/extensions/nlp-query/OpenAiManager.ts
@@ -107,16 +107,29 @@ export class OpenAiClient {
 
   getSystemMessage() {
     return `
-      Task: Generate Cypher queries to query a Neo4j graph database based on the provided schema definition. These queries will be used inside NeoDash reports.
-      Documentation for NeoDash is here : https://neo4j.com/labs/neodash/2.2/
-      Instructions:
-      Use only the provided relationship types and properties.
-      Do not use any other relationship types or properties that are not provided.
-      The Cypher RETURN clause must contained certain variables, based on the report type asked for.
-      Report types :
-      Single Value - A single value of a single variable
-      Pie Chart - Two variables named category and value
-      Sankey - Three variables, two being a node object (and not a property value) and one representing a relationship object (and not a property value).
+    Task: Generate Cypher queries to query a Neo4j graph database based on the provided schema definition. These queries will be used inside NeoDash reports.
+    Documentation for NeoDash is here : https://neo4j.com/labs/neodash/2.2/
+    Instructions:
+    Use only the provided relationship types and properties.
+    Do not use any other relationship types or properties that are not provided.
+    The Cypher RETURN clause must contained certain variables, based on the report type asked for.
+    Report types :
+    Table - Multiple variables representing property values of nodes and relationships.
+    Graph - Multiple variables representing property values of nodes and relationships or the node objects and relationship objects.
+    Bar Chart - Two variables named category(a String value) and value(numeric value).
+    Line Chart - Two numeric variables named x and y.
+    Sunburst - Two variables named Path(list of strings) and value(a numerical value).
+    Circle Packing - Two variables named Path(a list of strings) and value(a numerical value).
+    Choropleth - Two variables named code(a String value) and value(a numerical value).
+    Area Map - Two variables named code(a String value) and value(a numerical value).
+    Treemap - Two variables named Path(a list of strings) and value(a numerical value).
+    Radar Chart - Multiple variables representing property values of nodes and relationships.
+    Sankey Chart - Three variables, two being a node object (and not a property value) and one representing a relationship object (and not a property value).
+    Map - multiple variables representing nodes objects(should contain spatial propeties) and relationship objects.
+    Single Value - A single value of a single variable.
+    Gauge Chart - A single value of a single variable.
+    Raw JSON - The Cypher query must return a JSON object that will be displayed as raw JSON data.
+    Pie Chart - Two variables named category and value.
       Schema:
       ${this.schemaText}
     `;
