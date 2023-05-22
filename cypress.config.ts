33812e13d6fd3fc009e366997447dbc58f8cdb2a
Bennu <agudeloharold13@gmail.com>
2023-05-23 01:56:36 +0200

    no im

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,11 +5,6 @@ export default defineConfig({
   video: false,
   e2e: {
     supportFile: false,
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
-    },
     baseUrl: 'http://localhost:3000',
   },
 });
