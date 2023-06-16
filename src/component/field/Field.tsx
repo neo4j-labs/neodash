10348273ddda16c5af9fef1d2affb01f4bd53dfa
Marius Conjeaud <rmcj@novonordisk.com>
2023-06-16 15:21:02 +0200

    Make multiline Field use Textarea

--- src/component/field/Field.tsx
+++ src/component/field/Field.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dropdown, TextInput } from '@neo4j-ndl/react';
+import { Dropdown, TextInput, Textarea } from '@neo4j-ndl/react';
 
 const textFieldStyle = { width: '155px', marginBottom: '10px', marginRight: '10px', marginLeft: '10px' };
 
@@ -40,6 +40,36 @@ const NeoField = ({
         size={size}
       ></Dropdown>
     </div>
+  ) : multiline === true ? (
+    <div style={style}>
+      <Textarea
+        key={label}
+        variant={variant}
+        label={label}
+        helpText={helperText}
+        disabled={disabled}
+        value={value != null ? value : defaultValue}
+        fluid
+        onClick={(e) => {
+          onClick(e.target.textContent);
+        }}
+        onChange={(event) => {
+          if (!numeric) {
+            onChange(event.target.value);
+          } else if (
+            event.target.value.toString().length == 0 ||
+            event.target.value.endsWith('.') ||
+            event.target.value.startsWith('-')
+          ) {
+            onChange(event.target.value);
+          } else if (!isNaN(event.target.value)) {
+            onChange(Number(event.target.value));
+          }
+        }}
+        placeholder={placeholder}
+        size={size}
+      ></Textarea>
+    </div>
   ) : (
     <div style={style}>
       <TextInput
@@ -49,7 +79,6 @@ const NeoField = ({
         helpText={helperText}
         disabled={disabled}
         value={value != null ? value : defaultValue}
-        multiline={multiline.toString()}
         fluid
         onClick={(e) => {
           onClick(e.target.textContent);
