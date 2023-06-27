96d99d3cd657955c8ab85c05de7ebc1c73ffa390
Alfred Rubin <alfredo.rubin@neo4j.com>
2023-06-27 16:08:49 +0200

    fixed wrong error throw in NeoGraphChartFitViewButton

--- src/chart/graph/component/button/GraphChartFitViewButton.tsx
+++ src/chart/graph/component/button/GraphChartFitViewButton.tsx
@@ -14,7 +14,6 @@ export const NeoGraphChartFitViewButton = (props: GraphChartVisualizationProps) 
         <FitToScreenIcon
           onClick={() => {
             props.interactivity.zoomToFit();
-            throw `Not Implemented${props}`;
           }}
         />
       </IconButton>
