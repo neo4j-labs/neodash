dfab3de76468b19a984f3c8587262a1f8273057e
Bennu <agudeloharold13@gmail.com>
2023-06-27 14:08:32 +0200

    t2

--- src/modal/WelcomeScreenModal.tsx
+++ src/modal/WelcomeScreenModal.tsx
@@ -39,7 +39,7 @@ export const NeoWelcomeScreenModal = ({
     <div>
       <Dialog size='small' open={welcomeScreenOpen == true} aria-labelledby='form-dialog-title' disableCloseButton>
         <Dialog.Header id='form-dialog-title'>
-          NeoDash - Neo4j Dashboard Builder
+          NeoDash - Neo4j Dashboard Builder 2
           <BoltIconSolid className='icon-base' color='gold' style={{ float: 'right' }} />
         </Dialog.Header>
         <Dialog.Content>
