04597c2ad561e09b9fa2bd55313531c9a8969982
Bennu <agudeloharold13@gmail.com>
2023-08-21 19:17:29 +0200

    Fixing unused import

--- src/component/field/ColorPicker.tsx
+++ src/component/field/ColorPicker.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { ColorPicker } from 'mui-color';
 
 const NeoColorPicker = ({ value, onChange, defaultValue, label, style }) => {
