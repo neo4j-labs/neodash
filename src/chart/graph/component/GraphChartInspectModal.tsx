2d09aae1254b37d6c99dfab80febe7b28b23b05f
Brahm Prakash Mishra <brahm.praksh_mishra@mercedes-benz.com>
2023-08-25 07:00:05 +0530

    Added source, target details in relationship detail view

--- src/chart/graph/component/GraphChartInspectModal.tsx
+++ src/chart/graph/component/GraphChartInspectModal.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { GraphChartVisualizationProps } from '../GraphChartVisualization';
-import { getEntityHeader } from '../util/NodeUtils';
+import { getEntityHeader, getEntityHeaderForEdge } from '../util/NodeUtils';
 import { Dialog } from '@neo4j-ndl/react';
 import GraphEntityInspectionTable from './GraphEntityInspectionTable';
 
@@ -8,6 +8,20 @@ import GraphEntityInspectionTable from './GraphEntityInspectionTable';
  * Renders a pop-up window to inspect a node/relationship properties in a read-only table.
  */
 export const NeoGraphChartInspectModal = (props: GraphChartVisualizationProps) => {
+  let headerName = '';
+  const propertySelections = props?.engine.selection ? props.engine.selection : {};
+  const selectedEntity = props.interactivity?.selectedEntity;
+
+  // Check if the user clicked relationship or edge
+  const isRelationshipOrEdge = selectedEntity ? Object.getOwnPropertyNames(selectedEntity).includes('type') : false;
+
+  if (selectedEntity) {
+    // Get header name of modal based on the node or edge clicked by user
+    headerName = isRelationshipOrEdge
+      ? getEntityHeaderForEdge(selectedEntity, propertySelections)
+      : getEntityHeader(selectedEntity);
+  }
+
   return (
     <div>
       <Dialog
@@ -16,9 +30,7 @@ export const NeoGraphChartInspectModal = (props: GraphChartVisualizationProps) =
         onClose={() => props.interactivity.setPropertyInspectorOpen(false)}
         aria-labelledby='form-dialog-title'
       >
-        <Dialog.Header id='form-dialog-title'>
-          {props.interactivity.selectedEntity ? getEntityHeader(props.interactivity.selectedEntity) : ''}
-        </Dialog.Header>
+        <Dialog.Header id='form-dialog-title'>{headerName}</Dialog.Header>
         <Dialog.Content>
           <GraphEntityInspectionTable entity={props.interactivity.selectedEntity}></GraphEntityInspectionTable>
         </Dialog.Content>
