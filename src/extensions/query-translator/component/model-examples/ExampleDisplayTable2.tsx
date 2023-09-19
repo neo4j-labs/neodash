998de21d40498e2cf2494a3a8d649bf1f002be0d
jacobbleakley-neo4j <jacob.bleakley@neo4j.com>
2023-09-19 18:08:36 +0100

    styling and pagination needed

--- src/extensions/query-translator/component/model-examples/ExampleDisplayTable2.tsx
+++ src/extensions/query-translator/component/model-examples/ExampleDisplayTable2.tsx
@@ -1,42 +1,68 @@
-import React from 'react'
-
+import React from 'react';
+import { TrashIconOutline, PencilSquareIconOutline } from '@neo4j-ndl/react/icons';
 import {
-  ColumnDef,
   createColumnHelper,
   flexRender,
   getCoreRowModel,
   useReactTable,
-  Table as ReactTable,
-  PaginationState,
-  getFilteredRowModel,
-  getPaginationRowModel,
-} from '@tanstack/react-table'
+} from '@tanstack/react-table';
+import { IconButton } from '@neo4j-ndl/react';
 
 type Example = {
-  question: string
-  example: string
+  question: string;
+  answer: string;
   index: number
-}
+};
 
+const RemoveButton = ({ onClick }) => (
+  <IconButton
+    className='n-float-right n-text-right'
+    style={{ color: 'red' }}
+    aria-label='remove'
+    onClick={onClick}
+    size='medium'
+    clean
+  >
+    <TrashIconOutline aria-label={'remove'} />
+  </IconButton>
+);
 
-const columnHelper = createColumnHelper<Example>()
+const EditButton = ({ onClick }) => (
+  <IconButton className='n-float-right n-text-right' onClick={onClick} aria-label={'edit'} size='medium' clean>
+    <PencilSquareIconOutline aria-label={'edit'} />
+  </IconButton>
+);
 
+function ExampleDisplayTable2({ examples, deleteModelExample, handleEdit }) {
+  const columnHelper = createColumnHelper<Example>();
 
-
-const ExampleDisplayTable2 = ({ examples, deleteModelExample, handleEdit }) => {
+  const columns = React.useMemo(
+    () => [
+      columnHelper.accessor('question', {
+        cell: (info) => info.getValue(),
+        header: () => <span>Question</span>,
+      }),
+      columnHelper.accessor('answer', {
+        cell: (info) => info.getValue(),
+        header: () => <span>Answer</span>,
+      }),
+      {
+        feader: '',
+        id: 'actions',
+        cell: ({ row }) => (
+          <div className='n-float-right n-text-right n-w-[100px]'>
+            <RemoveButton onClick={() => deleteModelExample(row.index)} />
+            <EditButton onClick={() => handleEdit(row.index)} />
+          </div>
+        ),
+      }
+ 
+    ],
+    []
+  );
 
   const data = React.useMemo(() => examples, [examples]);
-  const rerender = React.useReducer(() => ({}), {})[1]
-
-  const columns = React.useMemo<ColumnDef<Example>[]>(()=> [
-    columnHelper.accessor(‘question, {
-      // cell: info => info.getValue(),
-      header: () => <span>Question</span>,
-    }),
-    columnHelper.accessor(‘answer’, {
-      header: () => <span>Last Name</span>,
-    }),
-  ], []);
+  const rerender = React.useReducer(() => ({}), {})[1];
 
   const table = useReactTable({
     data,
@@ -45,40 +71,32 @@ const ExampleDisplayTable2 = ({ examples, deleteModelExample, handleEdit }) => {
   });
 
   return (
-    <div className="p-2">
+    <div className='p-2'>
       <table>
         <thead>
-          {table.getHeaderGroups().map(headerGroup => (
+          {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
-              {headerGroup.headers.map(header => (
+              {headerGroup.headers.map((header) => (
                 <th key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
+                  {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
                 </th>
               ))}
             </tr>
           ))}
         </thead>
         <tbody>
-          {table.getRowModel().rows.map(row => (
+          {table.getRowModel().rows.map((row) => (
             <tr key={row.id}>
-              {row.getVisibleCells().map(cell => (
-                <td key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
               ))}
             </tr>
           ))}
         </tbody>
       </table>
-      <div className="h-4" />
+      <div className='h-4' />
     </div>
   );
-};
+}
 
 export default ExampleDisplayTable2;
-
