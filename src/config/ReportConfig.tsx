de3316f426a7f6f819bacc9698bf7bcdc06c645d
Brahm Prakash Mishra <brahm.praksh_mishra@mercedes-benz.com>
2023-10-04 09:07:59 +0530

    Added options for bringing in more tree orientations and also an option to control the separation between various levels of a tree heirarchy (DagLevelDistance)

--- src/config/ReportConfig.tsx
+++ src/config/ReportConfig.tsx
@@ -241,8 +241,13 @@ export const REPORT_TYPES = {
       layout: {
         label: 'Graph Layout (experimental)',
         type: SELECTION_TYPES.LIST,
-        values: ['force-directed', 'tree', 'radial'],
+        values: ['force-directed', 'tree-top-down', 'tree-bottom-up', 'tree-left-right', 'tree-right-left', 'radial'],
         default: 'force-directed',
+      },
+      graphDepthSep: {
+        label: 'Graph Depth Separation (experimental)',
+        type: SELECTION_TYPES.NUMBER,
+        default: '30',
       },
       enableExploration: {
         label: 'Enable graph exploration',
