6aa88c2e349b77e2b375d8434ff170306485047e
Niels de Jong <niels-121@hotmail.com>
2023-11-01 10:00:33 +0100

    Clean up code structure

--- src/extensions/advancedcharts/chart/gantt/GanttChart.tsx
+++ src/extensions/advancedcharts/chart/gantt/GanttChart.tsx
@@ -7,7 +7,7 @@ import { categoricalColorSchemes } from '../../../../config/ColorConfig';
 import { CARD_HEADER_HEIGHT } from '../../../../config/CardConfig';
 import { extractNodePropertiesFromRecords } from '../../../../report/ReportRecordProcessing';
 import { extensionEnabled } from '../../../../utils/ReportUtils';
-import { generateVisualizationDataGraph } from './Utils';
+import { createDependenciesMap, createTasksList, generateVisualizationDataGraph } from './Utils';
 import ReactGantt from './frappe/GanttVisualization';
 
 /**
@@ -18,21 +18,30 @@ const NeoGanttChart = (props: ChartProps) => {
   const { records } = props;
   const { selection } = props;
   const settings = props.settings ? props.settings : {};
+
+  /**
+   * This is where we store an in-memory graph from the Neo4j results.
+   * We are essentially reconstructing the graph from the set of links, nodes, and paths returned in Cypher.
+   */
   const [data, setData] = useState({ nodes: [] as any[], links: [] as any[] });
+
+  //
   const startDateProperty = settings.startDateProperty ? settings.startDateProperty : 'startDate';
   const endDateProperty = settings.endDateProperty ? settings.endDateProperty : 'endDate';
-  const nameProperty = settings.nameProperty ? settings.nameProperty : 'activityName';
+  const nameProperty = settings.nameProperty ? settings.nameProperty : 'name';
 
   let nodeLabels = {};
   let linkTypes = {};
 
   const colorScheme = categoricalColorSchemes[settings.nodeColorScheme];
+
+  // Get the set of report actions defined for the report.
   const actionsRules =
     extensionEnabled(props.extensions, 'actions') && props.settings && props.settings.actionsRules
       ? props.settings.actionsRules
       : [];
 
-  // When data is refreshed, rebuild the visualization data.
+  // When data is refreshed, rebuild the data graph used for visualization.
   useEffect(() => {
     setData(
       generateVisualizationDataGraph(props.records, nodeLabels, linkTypes, colorScheme, props.fields, props.settings)
@@ -46,60 +55,9 @@ const NeoGanttChart = (props: ChartProps) => {
     return <NoDrawableDataErrorMessage />;
   }
 
-  // Helper function to extract a dependency map from the parsed relationships.
-  function createDependenciesMap(links) {
-    const dependencies = {};
-    links.forEach((l) => {
-      if (!dependencies[l.target]) {
-        dependencies[l.target] = [];
-      }
-      dependencies[l.target].push(l.source);
-    });
-    return dependencies;
-  }
-
-  // Helper function to extract a list of task objects from the parsed nodes.
-  function createTasksList(nodes, dependencies) {
-    return nodes
-      .map((n) => {
-        const neoStartDate = n.properties[startDateProperty];
-        const neoEndDate = n.properties[endDateProperty];
-        const name = n.properties[nameProperty];
-
-        // If any of the dates cannot be parsed, we do not visualize this node.
-        if (
-          !neoStartDate ||
-          !neoStartDate.year ||
-          !neoStartDate.month ||
-          !neoStartDate.day ||
-          !neoEndDate ||
-          !neoEndDate.year ||
-          !neoEndDate.month ||
-          !neoEndDate.day
-        ) {
-          return undefined;
-        }
-        return {
-          start: new Date(neoStartDate.year, neoStartDate.month, neoStartDate.day),
-          end: new Date(neoEndDate.year, neoEndDate.month, neoEndDate.day),
-          name: name ? name : '(undefined)',
-          dependencies: dependencies[n.id],
-          id: `${  n.id}`,
-          properties: n.properties,
-          labels: n.labels,
-          type: 'task',
-          progress: 100,
-          // custom_class: 'bar-milestone',
-          isDisabled: true,
-          styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d' },
-        };
-      })
-      .filter((i) => i !== undefined);
-  }
-
   // Build visualization-specific objects.
   const dependencies = createDependenciesMap(data.links);
-  const tasks: Task[] = createTasksList(data.nodes, dependencies);
+  const tasks = createTasksList(data.nodes, dependencies, startDateProperty, endDateProperty, nameProperty);
 
   // If no tasks can be parsed, also return an error message.
   if (!tasks || tasks.length == 0) {
@@ -123,57 +81,6 @@ const NeoGanttChart = (props: ChartProps) => {
   let dateDiff = (maxDate - minDate) / (1000 * 60 * 60 * 24);
 
   const viewMode = dateDiff > 40 ? 'Month' : 'Day';
-
-  let tasksa = [
-    {
-      start: '2018-10-01',
-      end: '2018-10-08',
-      name: 'Redesign website',
-      id: 'Task 0',
-      progress: 20,
-    },
-    {
-      start: '2018-10-03',
-      end: '2018-10-06',
-      name: 'Write new content',
-      id: 'Task 1',
-      progress: 5,
-      dependencies: 'Task 0',
-    },
-    {
-      start: '2018-10-04',
-      end: '2018-10-08',
-      name: 'Apply new styles',
-      id: 'Task 2',
-      progress: 10,
-      dependencies: 'Task 1',
-    },
-    {
-      start: '2018-10-08',
-      end: '2018-10-09',
-      name: 'Review',
-      id: 'Task 3',
-      progress: 5,
-      dependencies: 'Task 2',
-    },
-    {
-      start: '2018-10-08',
-      end: '2018-10-10',
-      name: 'Deploy',
-      id: 'Task 4',
-      progress: 0,
-      dependencies: 'Task 2',
-    },
-    {
-      start: '2018-10-11',
-      end: '2018-10-11',
-      name: 'Go Live!',
-      id: 'Task 5',
-      progress: 0,
-      dependencies: 'Task 4',
-      custom_class: 'bar-milestone',
-    },
-  ];
 
   return (
     <div
--- src/extensions/advancedcharts/chart/gantt/Utils.ts
+++ src/extensions/advancedcharts/chart/gantt/Utils.ts
@@ -24,3 +24,54 @@ export const generateVisualizationDataGraph = (records, nodeLabels, linkTypes, c
   );
   return extractedGraphFromRecords;
 };
+
+// Helper function to extract a dependency map from the parsed relationships.
+export function createDependenciesMap(links) {
+  const dependencies = {};
+  links.forEach((l) => {
+    if (!dependencies[l.target]) {
+      dependencies[l.target] = [];
+    }
+    dependencies[l.target].push(l.source);
+  });
+  return dependencies;
+}
+
+// Helper function to extract a list of task objects from the parsed nodes.
+export function createTasksList(nodes, dependencies, startDateProperty, endDateProperty, nameProperty) {
+  return nodes
+    .map((n) => {
+      const neoStartDate = n.properties[startDateProperty];
+      const neoEndDate = n.properties[endDateProperty];
+      const name = n.properties[nameProperty];
+
+      // If any of the dates cannot be parsed, we do not visualize this node.
+      if (
+        !neoStartDate ||
+        !neoStartDate.year ||
+        !neoStartDate.month ||
+        !neoStartDate.day ||
+        !neoEndDate ||
+        !neoEndDate.year ||
+        !neoEndDate.month ||
+        !neoEndDate.day
+      ) {
+        return undefined;
+      }
+      return {
+        start: new Date(neoStartDate.year, neoStartDate.month, neoStartDate.day),
+        end: new Date(neoEndDate.year, neoEndDate.month, neoEndDate.day),
+        name: name ? name : '(undefined)',
+        dependencies: dependencies[n.id],
+        id: `${n.id}`,
+        properties: n.properties,
+        labels: n.labels,
+        type: 'task',
+        progress: 100,
+        // custom_class: 'bar-milestone',
+        isDisabled: true,
+        styles: { progressColor: '#ffbb54', progressSelectedColor: '#ff9e0d' },
+      };
+    })
+    .filter((i) => i !== undefined);
+}
