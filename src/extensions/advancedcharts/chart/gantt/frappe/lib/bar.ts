d2464c4d6833b83afe1e2d421384a3cc54186ef6
Niels de Jong <niels-121@hotmail.com>
2023-11-13 12:54:03 +0100

    Updated label positions

--- src/extensions/advancedcharts/chart/gantt/frappe/lib/bar.ts
+++ src/extensions/advancedcharts/chart/gantt/frappe/lib/bar.ts
@@ -152,7 +152,7 @@ export default class Bar {
 
   draw_label() {
     createSVG('text', {
-      x: this.x + this.width / 2,
+      x: this.x + this.width + 5,
       y: this.y + this.height / 2,
       innerHTML: this.task.name,
       class: 'bar-label',
@@ -412,7 +412,7 @@ export default class Bar {
       label.setAttribute('x', bar.getX() + bar.getWidth() + 5);
     } else {
       label.classList.remove('big');
-      label.setAttribute('x', bar.getX() + bar.getWidth() / 2);
+      label.setAttribute('x', bar.getX() + bar.getWidth() + 5);
     }
   }
 
