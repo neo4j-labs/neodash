826e0e00bb911dcf285d60cef2004561785f9f03
alfredorubin96 <103421036+alfredorubin96@users.noreply.github.com>
2023-11-16 13:09:08 +0100

    Fix hiding column prefix (#695)
    * fixing missing feature that was hiding columns starting with __
    
    * removing test console log
    
    * removing code smell
    
    * removing code smell
    
    * fixing test
    
    * Bumped outdated version numbers
    
    ---------
    
    Co-authored-by: Alfred Rubin <alfredo.rubin@neo4j.com>
    Co-authored-by: Niels de Jong <niels-121@hotmail.com>

--- cypress/e2e/start_page.cy.js
+++ cypress/e2e/start_page.cy.js
@@ -87,7 +87,7 @@ describe('NeoDash E2E Tests', () => {
     cy.get('main .react-grid-item:eq(2) .MuiDataGrid-columnHeaders', { timeout: WAITING_TIME })
       .should('contain', 'title')
       .and('contain', 'released')
-      .and('contain', '__id');
+      .and('not.contain', '__id');
     cy.get('main .react-grid-item:eq(2) .MuiDataGrid-virtualScroller .MuiDataGrid-row').should('have.length', 5);
     cy.get('main .react-grid-item:eq(2) .MuiDataGrid-footerContainer').should('contain', '1–5 of 8');
     cy.get('main .react-grid-item:eq(2) .MuiDataGrid-footerContainer button[aria-label="Go to next page"]').click();
