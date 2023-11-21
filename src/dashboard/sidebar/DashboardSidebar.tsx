5a95e1dad59910c119197a9d4a5fccf72a498186
Alfred Rubin <alfredo.rubin@neo4j.com>
2023-11-21 17:22:28 +0100

    removing useless import

--- src/dashboard/sidebar/DashboardSidebar.tsx
+++ src/dashboard/sidebar/DashboardSidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { connect } from 'react-redux';
 import { getDashboardIsEditable, getPageNumber } from '../../settings/SettingsSelectors';
 import { getDashboardSettings, getDashboardTitle } from '../DashboardSelectors';
