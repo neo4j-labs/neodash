7137449873ce4939898a469029a6a3afe66336fb
Monish <139316519+m-o-n-i-s-h@users.noreply.github.com>
2023-12-08 14:13:46 +0530

    Added landing page link to logo

--- src/dashboard/header/DashboardHeaderLogo.tsx
+++ src/dashboard/header/DashboardHeaderLogo.tsx
@@ -8,13 +8,10 @@ await StyleConfig.getInstance();
 
 export const NeoDashboardHeaderLogo = () => {
   const content = (
-    <div
-      className='n-items-center sm:n-flex md:n-flex-1 n-justify-start'
-      onClick={() => window.open('/landing-page/', '_blank')}
-    >
-      {/* <a className='n-cursor-pointer'> */}
-      <img className='n-h-10 n-w-auto n-m-2' src={'update-paths-tool-logo.png'} alt='Logo' />
-      {/* </a> */}
+    <div className='n-items-center sm:n-flex md:n-flex-1 n-justify-start n-cursor-pointer'>
+      <a href='/landing-page/' target='_blank'>
+        <img className='n-h-10 n-w-auto n-m-2' src={'update-paths-tool-logo.png'} alt='Logo' />
+      </a>
       {/* {IS_CUSTOM_LOGO ? <></> : <Typography variant='h6'>Labs</Typography>} */}
     </div>
   );
