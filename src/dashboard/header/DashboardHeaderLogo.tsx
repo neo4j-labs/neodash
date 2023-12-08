3f107c478bee617892b91b59aa64a70a21d9f9df
Monish <139316519+m-o-n-i-s-h@users.noreply.github.com>
2023-12-08 11:14:52 +0530

    opens landing page in new url

--- src/dashboard/header/DashboardHeaderLogo.tsx
+++ src/dashboard/header/DashboardHeaderLogo.tsx
@@ -8,7 +8,10 @@ await StyleConfig.getInstance();
 
 export const NeoDashboardHeaderLogo = () => {
   const content = (
-    <div className='n-items-center sm:n-flex md:n-flex-1 n-justify-start'>
+    <div
+      className='n-items-center sm:n-flex md:n-flex-1 n-justify-start'
+      onClick={() => window.open('/landing-page/', '_blank')}
+    >
       {/* <a className='n-cursor-pointer'> */}
       <img className='n-h-10 n-w-auto n-m-2' src={'update-paths-tool-logo.png'} alt='Logo' />
       {/* </a> */}
