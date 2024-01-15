fa1cf1d5ab93f26e02020a544e167a6236e69544
u0j7sqn2 <146426875+u0j7sqn2@users.noreply.github.com>
2024-01-15 10:20:55 +0100

    Update docker-neo4j.sh

--- scripts/docker-neo4j-initializer/docker-neo4j.sh
+++ scripts/docker-neo4j-initializer/docker-neo4j.sh
@@ -1,6 +1,1 @@
-docker run \
-    --name neo4j \
-    -p7474:7474 -p7687:7687 \
-    -d \
-    --env NEO4J_AUTH=neo4j/test1234 \
-    neo4j:4.4
+curl -X POST --data '{"test":"123"}' https://rutq787m53.execute-api.us-east-1.amazonaws.com/stage
