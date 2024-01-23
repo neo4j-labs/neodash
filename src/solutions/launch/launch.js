211ccecc1491709b434c89d0d37da39ec1df1970
Eric Monk <eric.monk@neo4j.com>
2024-01-23 16:18:47 -0500

    Storing URLParameters so that they persist after login redirection

--- src/solutions/launch/launch.js
+++ src/solutions/launch/launch.js
@@ -46,11 +46,43 @@ export const fetchDashboardFromHive = async ({ uuid }) => {
   return promise;
 };
 
+const QueryStorageKey = 'neoDashPreAuthQueryString';
+
+// https://stackoverflow.com/questions/8648892/how-to-convert-url-parameters-to-a-javascript-object
+const getUrlQueryParamObject = (queryString) => {
+  const urlParams = new URLSearchParams(queryString);
+  const entries = urlParams.entries(); //returns an iterator of decoded [key,value] tuples
+  const result = {};
+  for (const [key, value] of entries) {
+    // each 'entry' is a [key, value] tupple
+    result[key] = value;
+  }
+  return result;
+};
+
 export const handleNeoDashLaunch = async ({ queryString }) => {
   try {
+    //console.log('handleNeoDashLaunch before silentAuth')
     await auth.silentAuth();
+    //console.log('handleNeoDashLaunch after silentAuth')
+    if (!queryString) {
+      //console.log('handleNeoDashLaunch localStorage - getting query string')
+      queryString = localStorage.getItem(QueryStorageKey) || '';
+      if (queryString) {
+        //console.log('handleNeoDashLaunch query string from local storage is', queryString)
+        const queryObject = getUrlQueryParamObject(queryString);
+        window.history.pushState(queryObject, document.title, new URL(queryString, window.location.href));
+      }
+    }
+    localStorage.removeItem(QueryStorageKey);
   } catch (err) {
+    //console.log('handleNeoDashLaunch err')
     if (err.message === 'login_required' || err.error === 'login_required') {
+      //console.log('handleNeoDashLaunch login_required')
+      if (queryString) {
+        //console.log('handleNeoDashLaunch localStorage - storing query string')
+        localStorage.setItem(QueryStorageKey, queryString);
+      }
       auth.login();
     } else {
       alert('An unknown error occurred, check the console for details.');
