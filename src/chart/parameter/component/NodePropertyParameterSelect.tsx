8976a6115fb1c0b58621d283a32d8e44c0f6d671
Alfred Rubin <alfredo.rubin@neo4j.com>
2024-02-26 19:11:29 +0100

    fixed two errors: on initialization two values of which one was empty and input.toLowerCase is not a function error

--- src/chart/parameter/component/NodePropertyParameterSelect.tsx
+++ src/chart/parameter/component/NodePropertyParameterSelect.tsx
@@ -103,13 +103,15 @@ const NodePropertyParameterSelectComponent = (props: ParameterSelectProps) => {
     let valDisplayReference = manualParameterSave ? paramValueDisplayLocal : props.parameterDisplayValue;
     // Multiple and new entry
     if (isMulti && inputValue !== null && newDisplay !== null && inputValue.length < newDisplay.length) {
-      newValue = Array.isArray(valReference) ? [...valReference] : [valReference];
+      newValue = Array.isArray(valReference)
+        ? [...valReference]
+        : valReference && valReference !== null
+        ? [valReference]
+        : [];
       const newDisplayValue = [...newDisplay].slice(-1)[0];
-
       let val = extraRecords.filter((r) => r._fields[displayValueRowIndex].toString() == newDisplayValue)[0]._fields[
         realValueRowIndex
       ];
-
       if (newValue.low) {
         newValue.push(toNumber(val));
       } else {
@@ -130,7 +132,6 @@ const NodePropertyParameterSelectComponent = (props: ParameterSelectProps) => {
 
     setInputDisplayText(isMulti ? '' : newDisplay);
     setInputValue(newDisplay);
-
     handleParametersUpdate(newValue, newDisplay, manualParameterSave);
   };
 
@@ -164,7 +165,6 @@ const NodePropertyParameterSelectComponent = (props: ParameterSelectProps) => {
       />
     );
   }
-
   return (
     <div className={'n-flex n-flex-row n-flex-wrap n-items-center'}>
       <Autocomplete
@@ -179,7 +179,7 @@ const NodePropertyParameterSelectComponent = (props: ParameterSelectProps) => {
           marginLeft: '15px',
           marginTop: '5px',
         }}
-        inputValue={inputDisplayText || ''}
+        inputValue={inputDisplayText.toString() || ''}
         onInputChange={(event, value) => {
           setInputDisplayText(value);
           debouncedQueryCallback(props.query, { input: `${value}`, ...allParameters }, setExtraRecords);
