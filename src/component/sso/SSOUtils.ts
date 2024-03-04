3e182818842da9ce7c7a2d5d64bc36bd2a6f1801
Niels de Jong <Niels-121@hotmail.com>
2024-03-04 11:39:19 +0100

    Revert "Handle token expiration in SSO (#611)"
    This reverts commit 001cee1cfe80a21f7d348cea03a74e1328cbe5df.

--- src/component/sso/SSOUtils.ts
+++ src/component/sso/SSOUtils.ts
@@ -118,7 +118,7 @@ export const initializeSSO = async (cachedSSODiscoveryUrl, _setCredentials) => {
       // Successful credentials retrieval.
       // Log in at the Neo4j dbms now using the Neo4j (js) driver.
       //
-      _setCredentials(credentials, mergedSSOProviders);
+      _setCredentials(credentials);
 
       // Exemplifying retrieval of stored URL paramenters
       _retrieveAdditionalURLParameters();
