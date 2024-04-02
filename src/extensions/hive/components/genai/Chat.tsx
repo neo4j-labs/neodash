d5744b3a6c95352ad7f8908f1bea775fb5706779
Eric Monk <eric.monk@neo4j.com>
2024-04-02 12:53:20 -0400

    Fixed async config issue when loading Chat

--- src/extensions/hive/components/genai/Chat.tsx
+++ src/extensions/hive/components/genai/Chat.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 
 import ForumIcon from '@mui/icons-material/Forum';
 import CloseIcon from '@mui/icons-material/Close';
@@ -6,150 +6,173 @@ import OpenInFullIcon from '@mui/icons-material/OpenInFull';
 import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
 import { Button } from '@mui/material';
 
-import { config } from '../../config/dynamicConfig';
+import { config, loadConfig } from '../../config/dynamicConfig';
 
-export default function Chat() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const chatEnabledUrlString = urlParams.get('genAiChatEnabled');
-  const genAiUrlString = urlParams.get('genAiUrl');
-
-  let chatEnabled = config('GenAIChatEnabled');
-  if (chatEnabledUrlString) {
-    if (chatEnabledUrlString === 'true') {
-      chatEnabled = true;
-    } else {
-      chatEnabled = false;
-    }
-  }
-
-  let genAiUrl = config('GenAIURL');
-  if (genAiUrlString) {
-    genAiUrl = genAiUrlString;
-  }
-
-  const [fullScreen, setFullScreen] = useState(false);
-  const [chatVisible, setChatVisible] = useState('hidden');
-
-  const toggleChat = () => {
-    if (chatVisible === 'hidden') {
-      setChatVisible('visible');
-    } else if (chatVisible === 'visible') {
-      setChatVisible('hidden');
-    }
+class Chat extends Component {
+  state = {
+    fullScreen: false,
+    chatVisible: 'hidden',
+    isInitialized: false,
   };
 
-  const toggleFullScreen = () => setFullScreen(!fullScreen);
+  async componentDidMount() {
+    this.auth = await loadConfig();
+    this.setState({ isInitialized: true });
+  }
 
-  return chatEnabled ? (
-    <>
-      <div
-        style={{
-          position: 'absolute',
-          display: 'flex',
-          visibility: chatVisible,
-          right: 20,
-          bottom: 60,
-          height: fullScreen ? 'calc(100vh - 140px)' : 'calc(50vh - 70px)',
-          width: '92vw',
-          zIndex: 101,
-          borderRadius: '5px',
-          border: '2px solid #ddd',
-          backgroundColor: '#fff',
-        }}
-      >
+  render() {
+    const { fullScreen, chatVisible, isInitialized } = this.state;
+
+    const setFullScreen = (value) => this.setState({ fullScreen: value });
+    const setChatVisible = (value) => this.setState({ chatVisible: value });
+
+    const urlParams = new URLSearchParams(window.location.search);
+
+    let chatEnabled = false;
+    let genAiUrl = '';
+
+    if (isInitialized) {
+      const chatEnabledUrlString = urlParams.get('genAiChatEnabled');
+      const genAiUrlString = urlParams.get('genAiUrl');
+
+      chatEnabled = config('GenAIChatEnabled');
+      if (chatEnabledUrlString) {
+        if (chatEnabledUrlString === 'true') {
+          chatEnabled = true;
+        } else {
+          chatEnabled = false;
+        }
+      }
+
+      genAiUrl = config('GenAIURL');
+      if (genAiUrlString) {
+        genAiUrl = genAiUrlString;
+      }
+    }
+
+    const toggleChat = () => {
+      if (chatVisible === 'hidden') {
+        setChatVisible('visible');
+      } else if (chatVisible === 'visible') {
+        setChatVisible('hidden');
+      }
+    };
+
+    const toggleFullScreen = () => setFullScreen(!fullScreen);
+
+    return chatEnabled && isInitialized ? (
+      <>
         <div
           style={{
+            position: 'absolute',
             display: 'flex',
-            flexFlow: 'column',
-            width: '100%',
+            visibility: chatVisible,
+            right: 20,
+            bottom: 60,
+            height: fullScreen ? 'calc(100vh - 140px)' : 'calc(50vh - 70px)',
+            width: '92vw',
+            zIndex: 101,
+            borderRadius: '5px',
+            border: '2px solid #ddd',
+            backgroundColor: '#fff',
           }}
         >
           <div
             style={{
-              flexGrow: 0,
               display: 'flex',
+              flexFlow: 'column',
               width: '100%',
-              height: '30px',
-              padding: '5px',
-              background: '#eee',
             }}
           >
-            <div
-              style={{
-                flexGrow: 1,
-                textAlign: 'left',
-                marginTop: '-2px',
-              }}
-            >
-              Chat
-            </div>
             <div
               style={{
                 flexGrow: 0,
                 display: 'flex',
-                flexFlow: 'row',
-                marginTop: '-2px',
-                cursor: 'pointer',
-                width: '50px',
+                width: '100%',
+                height: '30px',
+                padding: '5px',
+                background: '#eee',
               }}
             >
               <div
-                style={
-                  {
-                    // marginTop: '2px'
-                  }
-                }
-                onClick={() => toggleFullScreen()}
+                style={{
+                  flexGrow: 1,
+                  textAlign: 'left',
+                  marginTop: '-2px',
+                }}
               >
-                {fullScreen ? (
-                  <CloseFullscreenIcon style={{ fontSize: '1.1rem' }} />
-                ) : (
-                  <OpenInFullIcon style={{ fontSize: '1.1rem' }} />
-                )}
+                Chat
               </div>
-              <div onClick={() => setChatVisible('hidden')}>
-                <CloseIcon />
+              <div
+                style={{
+                  flexGrow: 0,
+                  display: 'flex',
+                  flexFlow: 'row',
+                  marginTop: '-2px',
+                  cursor: 'pointer',
+                  width: '50px',
+                }}
+              >
+                <div
+                  style={
+                    {
+                      // marginTop: '2px'
+                    }
+                  }
+                  onClick={() => toggleFullScreen()}
+                >
+                  {fullScreen ? (
+                    <CloseFullscreenIcon style={{ fontSize: '1.1rem' }} />
+                  ) : (
+                    <OpenInFullIcon style={{ fontSize: '1.1rem' }} />
+                  )}
+                </div>
+                <div onClick={() => setChatVisible('hidden')}>
+                  <CloseIcon />
+                </div>
               </div>
             </div>
-          </div>
-          <div
-            style={{
-              flexGrow: 1,
-              width: '100%',
-            }}
-          >
-            <iframe
+            <div
               style={{
+                flexGrow: 1,
                 width: '100%',
-                height: '100%',
-                borderWidth: '0px',
               }}
-              id='genAIChat'
-              title='GenAI Embed'
-              src={genAiUrl}
-            />
+            >
+              <iframe
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  borderWidth: '0px',
+                }}
+                id='genAIChat'
+                title='GenAI Embed'
+                src={genAiUrl}
+              />
+            </div>
           </div>
         </div>
-      </div>
-      <div
-        style={{
-          position: 'absolute',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          right: 20,
-          bottom: 20,
-          height: 40,
-          width: 100,
-          zIndex: 100,
-        }}
-      >
-        <Button variant='contained' color='primary' onClick={toggleChat} fullWidth startIcon={<ForumIcon />}>
-          Chat
-        </Button>
-      </div>
-    </>
-  ) : (
-    <></>
-  );
+        <div
+          style={{
+            position: 'absolute',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            right: 20,
+            bottom: 20,
+            height: 40,
+            width: 100,
+            zIndex: 100,
+          }}
+        >
+          <Button variant='contained' color='primary' onClick={toggleChat} fullWidth startIcon={<ForumIcon />}>
+            Chat
+          </Button>
+        </div>
+      </>
+    ) : (
+      <></>
+    );
+  }
 }
+
+export default Chat;
