d5d78df3560f9342747c002bd9ba7bea16fb7d62
AleSim94 <aleksandar.simeunovic94@gmail.com>
2024-04-09 15:30:00 +0200

    added new scrollable bar(inline style) + more robust way to update users with special chars in their name like for example  "@" and "<".

--- src/extensions/rbac/RBACManagementMenu.tsx
+++ src/extensions/rbac/RBACManagementMenu.tsx
@@ -64,7 +64,7 @@ export const RBACManagementMenu = ({ anchorEl, MenuOpen, handleClose, createNoti
         onClose={handleClose}
         size='small'
       >
-        <MenuItems>
+        <MenuItems style={{ maxHeight: '200px', overflowY: 'auto' }}>
           {roles.map((role) => (
             <MenuItem key={role} onClick={() => handleRoleClicked(role)} icon={<UserIconOutline />} title={role} />
           ))}
