0432950c1fc6a21e6f67965d12d994200363d97e
Monish <139316519+m-o-n-i-s-h@users.noreply.github.com>
2024-06-21 15:04:47 +0530

    feat: Updated JSDoc

--- src/utils/parameterUtils.ts
+++ src/utils/parameterUtils.ts
@@ -1,4 +1,10 @@
-export const extractAllParameterNames = (cypherQuery) => {
+/**
+ * Extracts all parameter names from a given Cypher query string.
+ *
+ * @param {string} cypherQuery The Cypher query string to extract parameter names from.
+ * @returns {string[]} An array containing all extracted parameter names.
+ */
+export const extractAllParameterNames = (cypherQuery: string): string[] => {
     // A regular expression pattern to match parameter names following '$'
     const pattern = /\$([A-Za-z_]\w*)/g;
 
@@ -12,7 +18,14 @@ export const extractAllParameterNames = (cypherQuery) => {
     return parameterNames;
 }
 
-export const checkParametersNameInGlobalParameter = (parameterNames: string[], globalParameterNames: any) => {
+/**
+ * Checks if all parameter names are present in the global parameter names.
+ * 
+ * @param {string[]} parameterNames An array of parameter names to be checked.
+ * @param {object} globalParameterNames The object containing global parameter names to compare against.
+ * @returns {boolean} A boolean indicating whether all parameters are present in the global parameters.
+ */
+export const checkParametersNameInGlobalParameter = (parameterNames: string[], globalParameterNames: any): boolean => {
     for (const key of parameterNames) {
         if (!globalParameterNames[key] || (Array.isArray(globalParameterNames[key]) && globalParameterNames[key].length === 0) || globalParameterNames[key] === '') {
             return true;
