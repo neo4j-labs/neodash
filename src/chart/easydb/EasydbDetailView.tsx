5447ce470e7073dcda0e9f40345a5afacc95b177
Dominic Kempf <dominic.kempf@iwr.uni-heidelberg.de>
2024-09-24 09:15:43 +0200

    WIP Attempts

--- src/chart/easydb/EasydbDetailView.tsx
+++ src/chart/easydb/EasydbDetailView.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ChartProps } from '../Chart';
 
 const EasydbDetailView = (props: ChartProps) => {
+    useEffect(() => {
+        let ele = document.getElementById("easydb");
+        if (ele !== null) {
+            var link = document.createElement("link");
+            link.setAttribute("rel", "stylesheet");
+            link.setAttribute(
+                "href",
+                "easydb.css"
+            );
+            ele.shadowRoot.appendChild(link);
+            }
+    }, []);
+
    const records = props.records;
    return records.map(r => {
-       return <div class="w-[800px] overflow-y-auto"><easydb-detail-view uuid={r._fields[0]}/></div>
+       return <div class="w-[800px] overflow-y-auto"><easydb-detail-view id="easydb" uuid={r._fields[0]}/></div>
    })
 }
 
