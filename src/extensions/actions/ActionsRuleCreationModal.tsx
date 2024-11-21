08cce851305cbe694b287e823c5a14e4a303d6d1
Monish Nandakumaran <monish.nandakumaran@mercedes-benz.com>
2024-11-21 11:38:56 +0530

    refactored

--- src/extensions/actions/ActionsRuleCreationModal.tsx
+++ src/extensions/actions/ActionsRuleCreationModal.tsx
@@ -577,113 +577,115 @@ export const NeoCustomReportActionsModal = ({
                   </td>
                 </tr>
               </table>
-              <table>
-                <tr>
-                  <td colSpan={7}>
-                    <tr>
-                      <th colSpan={7} className='n-text-center n-font-bold n-py-2'>
-                        Report Pre Conditions
-                      </th>
-                    </tr>
-                  </td>
-                </tr>
-                {preConditions.map((con, i) => {
-                  return (
-                    <tr>
-                      <td width='2.5%' className='n-pr-1'>
-                        <span className='n-pr-1'>{i + 1}.</span>
-                        <span className='n-font-bold'>IF</span>
-                      </td>
-                      <td width='100%'>
-                        <div style={{ border: '2px dashed grey' }} className='n-p-1'>
-                          <Autocomplete
-                            className='n-align-middle n-inline-block n-w-5/12 n-pr-1'
-                            disableClearable={true}
-                            id={`autocomplete-label-type${i}`}
-                            size='small'
-                            noOptionsText='*Specify an exact field name'
-                            options={createFieldVariableSuggestions(null, null, null).filter((e) =>
-                              e.toLowerCase().includes(con.field.toLowerCase())
-                            )}
-                            value={con.field ? con.field : ''}
-                            inputValue={con.field ? con.field : ''}
-                            popupIcon={<></>}
-                            style={{ minWidth: 125 }}
-                            onInputChange={(event, value) => {
-                              updatePreConditionFieldById(i, 'field', value);
-                            }}
-                            onChange={(event, newValue) => {
-                              updatePreConditionFieldById(i, 'field', newValue);
-                            }}
-                            renderInput={(params) => (
-                              <TextField
-                                {...params}
-                                placeholder='Field name...'
-                                InputLabelProps={{ shrink: true }}
-                                style={{ padding: '6px 0 7px' }}
-                                size={'small'}
-                              />
-                            )}
-                          />
-                          <Dropdown
-                            type='select'
-                            className='n-align-middle n-w-2/12 n-pr-1'
-                            selectProps={{
-                              onChange: (newValue) => updatePreConditionFieldById(i, 'condition', newValue?.value),
-                              options: PRE_CONDITIONS_RULES.map((option) => ({
-                                label: option.label,
-                                value: option.value,
-                              })),
-                              value: { label: con.condition, value: con.condition },
-                            }}
-                            style={{ minWidth: 70, display: 'inline-block' }}
-                            fluid
-                          />
-                          <TextInput
-                            className='n-align-middle n-inline-block n-w-5/12'
-                            style={{ minWidth: 100 }}
-                            placeholder='Value...'
-                            value={con.value}
-                            onChange={(e) => updatePreConditionFieldById(i, 'value', e.target.value)}
-                            fluid
-                          ></TextInput>
-                        </div>
-                      </td>
+              {rules.some((rule) => rule?.condition === 'rowCheck') && (
+                <table>
+                  <tr>
+                    <td colSpan={7}>
+                      <tr>
+                        <th colSpan={7} className='n-text-center n-font-bold n-py-2'>
+                          Report Pre Conditions
+                        </th>
+                      </tr>
+                    </td>
+                  </tr>
+                  {preConditions.map((con, i) => {
+                    return (
+                      <tr>
+                        <td width='2.5%' className='n-pr-1'>
+                          <span className='n-pr-1'>{i + 1}.</span>
+                          <span className='n-font-bold'>IF</span>
+                        </td>
+                        <td width='100%'>
+                          <div style={{ border: '2px dashed grey' }} className='n-p-1'>
+                            <Autocomplete
+                              className='n-align-middle n-inline-block n-w-5/12 n-pr-1'
+                              disableClearable={true}
+                              id={`autocomplete-label-type${i}`}
+                              size='small'
+                              noOptionsText='*Specify an exact field name'
+                              options={createFieldVariableSuggestions(null, null, null).filter((e) =>
+                                e.toLowerCase().includes(con.field.toLowerCase())
+                              )}
+                              value={con.field ? con.field : ''}
+                              inputValue={con.field ? con.field : ''}
+                              popupIcon={<></>}
+                              style={{ minWidth: 125 }}
+                              onInputChange={(event, value) => {
+                                updatePreConditionFieldById(i, 'field', value);
+                              }}
+                              onChange={(event, newValue) => {
+                                updatePreConditionFieldById(i, 'field', newValue);
+                              }}
+                              renderInput={(params) => (
+                                <TextField
+                                  {...params}
+                                  placeholder='Field name...'
+                                  InputLabelProps={{ shrink: true }}
+                                  style={{ padding: '6px 0 7px' }}
+                                  size={'small'}
+                                />
+                              )}
+                            />
+                            <Dropdown
+                              type='select'
+                              className='n-align-middle n-w-2/12 n-pr-1'
+                              selectProps={{
+                                onChange: (newValue) => updatePreConditionFieldById(i, 'condition', newValue?.value),
+                                options: PRE_CONDITIONS_RULES.map((option) => ({
+                                  label: option.label,
+                                  value: option.value,
+                                })),
+                                value: { label: con.condition, value: con.condition },
+                              }}
+                              style={{ minWidth: 70, display: 'inline-block' }}
+                              fluid
+                            />
+                            <TextInput
+                              className='n-align-middle n-inline-block n-w-5/12'
+                              style={{ minWidth: 100 }}
+                              placeholder='Value...'
+                              value={con.value}
+                              onChange={(e) => updatePreConditionFieldById(i, 'value', e.target.value)}
+                              fluid
+                            ></TextInput>
+                          </div>
+                        </td>
 
-                      <td width='5%'>
+                        <td width='5%'>
+                          <IconButton
+                            aria-label='remove rule'
+                            size='medium'
+                            style={{ marginLeft: 10 }}
+                            floating
+                            onClick={() => {
+                              setPreConditions((prevItems) => prevItems.filter((_, j) => j !== i));
+                            }}
+                          >
+                            <XMarkIconOutline />
+                          </IconButton>
+                        </td>
+                      </tr>
+                    );
+                  })}
+
+                  <tr>
+                    <td colSpan={7}>
+                      <div className='n-text-center n-mt-1'>
                         <IconButton
-                          aria-label='remove rule'
+                          aria-label='add'
                           size='medium'
-                          style={{ marginLeft: 10 }}
                           floating
                           onClick={() => {
-                            setPreConditions((prevItems) => prevItems.filter((_, j) => j !== i));
+                            setPreConditions([...preConditions, defaultPreCondition]);
                           }}
                         >
-                          <XMarkIconOutline />
+                          <PlusIconOutline />
                         </IconButton>
-                      </td>
-                    </tr>
-                  );
-                })}
-
-                <tr>
-                  <td colSpan={7}>
-                    <div className='n-text-center n-mt-1'>
-                      <IconButton
-                        aria-label='add'
-                        size='medium'
-                        floating
-                        onClick={() => {
-                          setPreConditions([...preConditions, defaultPreCondition]);
-                        }}
-                      >
-                        <PlusIconOutline />
-                      </IconButton>
-                    </div>
-                  </td>
-                </tr>
-              </table>
+                      </div>
+                    </td>
+                  </tr>
+                </table>
+              )}
             </div>
           </Dialog.Content>
           <Dialog.Actions>
