cab7795167708458a511bca92a81d0514cbfea6e
“Bastien <bastien.hubert@hotmail.com>
2024-11-28 09:53:28 +0100

    Fix value.low existence check when value is not an Object

--- src/chart/ChartUtils.ts
+++ src/chart/ChartUtils.ts
@@ -164,7 +164,7 @@ export const downloadCSV = (rows) => {
     headers.forEach((header) => {
       // Parse value
       let value = row[header];
-      if (value && 'low' in value) {
+      if (value?.low !== undefined) {
         value = value.low;
       }
       csv += `${JSON.stringify(value)}`;
