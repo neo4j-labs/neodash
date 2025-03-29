2b6058f4c0378998354f24486cfe7a82e31e8ef8
Niels de Jong <niels-121@hotmail.com>
2025-03-29 08:25:28 +0100

    Allow remote loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,13 @@
+const { ModuleFederationPlugin } = require('webpack').container;
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
 const CircularDependencyPlugin = require('circular-dependency-plugin');
+const path = require('path');
 
 const circularPlugin = new CircularDependencyPlugin({
-  // exclude detection of files based on a RegExp
   exclude: /a\.js|node_modules/,
-  // add errors to webpack instead of warnings
   failOnError: false,
-  // allow import cycles that include an asyncronous import,
-  // e.g. via import(/* webpackMode: "weak" */ './file.js')
   allowAsyncCycles: false,
-  // set the current working directory for displaying module paths
   cwd: process.cwd(),
 });
 
@@ -23,7 +20,6 @@ const rules = [
     loader: 'babel-loader',
     options: {
       presets: ['@babel/env'],
-      //plugins: [isDevelopment && require.resolve('react-refresh/babel')].filter(Boolean),
     },
   },
   {
@@ -46,43 +42,55 @@ const rules = [
   },
 ];
 
-// TODO - move this config to a dedicated environment file.
-// TODO - use process.env.NODE_ENV which will directly return the environment string "development", "production".
 module.exports = (env) => {
   const production = env.production;
+
   return {
     experiments: {
       topLevelAwait: true,
     },
-    entry: ['./src/index.tsx'],
+    entry: './src/index.tsx',
     mode: production ? 'production' : 'development',
     devtool: production ? 'source-map' : 'eval-cheap-module-source-map',
-    module: {
-      rules: rules,
-    },
+    module: { rules },
     resolve: { extensions: ['*', '.js', '.jsx', '.ts', '.tsx'] },
     output: {
+      publicPath: 'auto',
+      path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.js',
     },
     devServer: {
       port: 3000,
+      host: '0.0.0.0', // Allow remote access
+      allowedHosts: 'all',
       hot: true,
       compress: true,
       client: {
-        overlay: {
-          warnings: false,
-        },
+        overlay: { warnings: false },
       },
     },
-    plugins: production
-      ? [
-          sentryWebpackPlugin({
-            authToken: process.env.SENTRY_AUTH_TOKEN,
-            org: 'neo4j-inc',
-            project: 'neodash',
-          }),
-        ]
-      : [new ReactRefreshWebpackPlugin(), ...(circularValidation ? [circularPlugin] : [])],
+    plugins: [
+      new ModuleFederationPlugin({
+        name: 'neodash',
+        filename: 'remoteEntry.js',
+        exposes: {
+          './Dashboard': './src/application/Application', // Modify as needed
+        },
+        shared: {
+          react: { singleton: true, eager: true, requiredVersion: '^17.0.2' },
+          'react-dom': { singleton: true, eager: true, requiredVersion: '^17.0.2' },
+        },
+      }),
+      ...(production
+        ? [
+            sentryWebpackPlugin({
+              authToken: process.env.SENTRY_AUTH_TOKEN,
+              org: 'neo4j-inc',
+              project: 'neodash',
+            }),
+          ]
+        : [new ReactRefreshWebpackPlugin(), ...(circularValidation ? [circularPlugin] : [])]),
+    ],
     ignoreWarnings: [/Failed to parse source map/],
   };
 };
